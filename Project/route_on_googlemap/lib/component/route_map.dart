8a3bf2ccb96069713c3ffa7bbee950ad7be139db
YONGJAEEE <lyj03152003@gmail.com>
2022-11-16 22:52:56 +0900

    June님 수정사항 반영, Warning & 주석 제거

--- Project/route_on_googlemap/lib/component/route_map.dart
+++ Project/route_on_googlemap/lib/component/route_map.dart
@@ -1,27 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class RouteMap extends StatelessWidget {
-  CameraPosition initialCameraPosition;
-  Set<Polyline> polylines;
-  RouteMap({required this.polylines, required this.initialCameraPosition, Key? key}) : super(key: key);
+class RouteMap extends StatefulWidget {
+  final CameraPosition initialCameraPosition;
+  final Set<Polyline> polylines;
 
+  const RouteMap({
+    required this.polylines,
+    required this.initialCameraPosition,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<RouteMap> createState() => _RouteMapState();
+}
+
+class _RouteMapState extends State<RouteMap> {
   GoogleMapController? mapController;
 
   onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
-    @override
+  @override
   Widget build(BuildContext context) {
     return GoogleMap(
       mapType: MapType.normal,
       myLocationEnabled: true,
       myLocationButtonEnabled: true,
       onMapCreated: onMapCreated,
-      initialCameraPosition: initialCameraPosition,
-      polylines: polylines,
+      initialCameraPosition: widget.initialCameraPosition,
+      polylines: widget.polylines,
     );
   }
 }
